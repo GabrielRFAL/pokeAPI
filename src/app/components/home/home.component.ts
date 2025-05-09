45be3256fdd9f08c167de966eb4ce9cbe8ac2b4c
Gabriel Ribas Ferreira de Almeida <gabrielribasfal@gmail.com>
2025-05-08 21:44:34 -0300

    styles: alterações em estilos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,22 @@
-// home.component.ts
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- IMPORTAÇÃO ADICIONADA
 import { PokeApiService } from '../../services/poke-api.service';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule] // <-- FormsModule ADICIONADO AQUI
 })
 export class HomeComponent implements OnInit {
 
   listaPokemons: any[] = [];
   listaImagensPokemons: any;
+  filtro: string = '';
 
   private apiUrlImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-
 
   constructor(private pokeApiService: PokeApiService) {}
 
@@ -36,6 +35,12 @@ export class HomeComponent implements OnInit {
   }
 
   getPokemonImageUrl(id: number): string {
-    return `${this.apiUrlImage}/${id}.png`;
+    return `${this.apiUrlImage}/${id}.png`; // <-- Corrigido para usar crase/template string
+  }
+
+  listaPokemonsFiltrados(): any[] {
+    return this.listaPokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(this.filtro.toLowerCase())
+    );
   }
 }
